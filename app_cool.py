7f822b845d33a17927880f49f52c984213ce95c8
codersavage <stemanant@gmail.com>
2025-09-13 20:19:27 -0400

    app cool

---
+++ app_cool.py
@@ -0,0 +1,5 @@
+1. Preference: what type of housing - deetya
+2. Input travel plans: what dates, what location - deetya
+3. Agent to propose staying options - yuga's api 
+4. Presenting each option one by one and peope can swipe left or right 
+5. Agent that uses swipes to create a stay per day  
